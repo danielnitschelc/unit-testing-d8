4388d9b183f9d6e04e74fe62ae387e455e70d39e
Daniel Nitsche <daniel.nitsche@loudclear.com.au>
2017-11-16 11:33:31 +1300

    Example DI / mock object.

--- web/modules/custom/quadrupaler/src/Quadrupaler.php
+++ web/modules/custom/quadrupaler/src/Quadrupaler.php
@@ -2,10 +2,22 @@
 
 namespace Drupal\quadrupaler;
 
+use Drupal\Core\Session\AccountProxy;
+
 class Quadrupaler {
 
+  private $accountProxy;
+
+  public function __construct(AccountProxy $accountProxy) {
+    $this->accountProxy = $accountProxy;
+  }
+
   public function quadrupal(string $string): string {
-    return str_replace('drupal', 'drupaldrupaldrupaldrupal', $string);
+    $replacement = str_replace('drupal', 'drupaldrupaldrupaldrupal', $string);
+    if (!$this->accountProxy->isAnonymous()) {
+      $replacement = ucfirst($replacement);
+    }
+    return $replacement;
   }
 
 }
--- web/modules/custom/quadrupaler/test/src/unit/QuadrupalerTest.php
+++ web/modules/custom/quadrupaler/test/src/unit/QuadrupalerTest.php
@@ -1,22 +1,38 @@
 <?php
 
+use Drupal\Core\Session\AccountProxy;
 use Drupal\quadrupaler\Quadrupaler;
 
 class QuadrupalerTest extends PHPUnit\Framework\TestCase {
 
+  /** @var PHPUnit_Framework_MockObject_MockObject $accountProxy */
+  private $accountProxy;
+
+  public function setUp() {
+    $this->accountProxy = $this->getMock(AccountProxy::class);
+  }
+
   public function testQuadrupal_GivenOneDrupal_ReturnFourDrupals() {
-    $quadrupaler = new Quadrupaler();
-    $this->assertEquals('drupaldrupaldrupaldrupal', $quadrupaler->quadrupal('drupal'));
+    $quadrupaler = new Quadrupaler($this->accountProxy);
+    $this->assertEquals('Drupaldrupaldrupaldrupal', $quadrupaler->quadrupal('drupal'));
   }
 
   public function testQuadrupal_GivenDRUPAL_ReturnDRUPAL() {
-    $quadrupaler = new Quadrupaler();
+    $quadrupaler = new Quadrupaler($this->accountProxy);
     $this->assertEquals('DRUPAL', $quadrupaler->quadrupal('DRUPAL'));
   }
 
   public function testQuadrupal_GivenDrupalWithText_ReturnFourDrupalsWithText() {
-    $quadrupaler = new Quadrupaler();
-    $this->assertEquals('adrupaldrupaldrupaldrupala', $quadrupaler->quadrupal('adrupala'));
+    $quadrupaler = new Quadrupaler($this->accountProxy);
+    $this->assertEquals('Adrupaldrupaldrupaldrupala', $quadrupaler->quadrupal('adrupala'));
+  }
+
+  public function testQuadrupal_GivenAuthenticatedUser_FirstCharUppercase() {
+    $this->accountProxy
+      ->method('isAnonymous')
+      ->willReturn(FALSE);
+    $quadrupaler = new Quadrupaler($this->accountProxy);
+    $this->assertEquals('Drupaldrupaldrupaldrupal', $quadrupaler->quadrupal('drupal'));
   }
 
 }
